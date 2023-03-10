8b997632d49596cd52e7c2de85778e30d740ce0b
Aidar <110410228+justreadthedocs@users.noreply.github.com>
2023-03-10 21:13:47 +0200

    2: Add editable Toast component preview

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -5,6 +5,7 @@ import VisuallyHidden from '../VisuallyHidden';
 
 import styles from './Toast.module.css';
 
+const VARIANTS = ['notice', 'warning', 'success', 'error'];
 const ICONS_BY_VARIANT = {
   notice: Info,
   warning: AlertTriangle,
@@ -12,14 +13,23 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Toast({ children, variant, onDismiss }) {
+  if (!VARIANTS.includes(variant)) {
+    throw new Error(`Invalid variant: ${variant}. Expected: ${VARIANTS.join(', ')}`);
+  }
+
+  const Icon = ICONS_BY_VARIANT[variant];
+
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Icon size={24} />
       </div>
-      <p className={styles.content}>16 photos have been uploaded</p>
-      <button className={styles.closeButton}>
+      <p className={styles.content}>{children}</p>
+      <button
+        className={styles.closeButton}
+        onClick={onDismiss}
+      >
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,6 +10,7 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState('notice');
+  const [toastIsVisible, setToastIsVisible] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -19,6 +21,17 @@ function ToastPlayground() {
         />
         <h1>Toast Playground</h1>
       </header>
+
+      {toastIsVisible && (
+        <Toast
+          variant={variant}
+          onDismiss={() => {
+            setToastIsVisible(false);
+          }}
+        >
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -68,7 +81,13 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button
+              onClick={() => {
+                setToastIsVisible(true);
+              }}
+            >
+              Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
